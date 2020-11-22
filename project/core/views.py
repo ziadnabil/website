3756f69f3d3cb1743c3dadf19527ab0323e6ae51
ziadnabil <ziadmoh2525@gmail.com>
2020-11-23 01:09:36 +0200

    get and post data using serializers

--- project/core/views.py
+++ project/core/views.py
@@ -1,15 +1,23 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.utils import serializer_helpers
 
 # third party imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
 
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            "name": "ziad",
-            "age": 22,
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
