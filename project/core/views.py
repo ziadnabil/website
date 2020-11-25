914cfff0d9336830d266c03e6994d6219b4a28da
ziadnabil <ziadmoh2525@gmail.com>
2020-11-25 23:33:51 +0200

    get post view

--- project/core/views.py
+++ project/core/views.py
@@ -5,11 +5,16 @@ from rest_framework.utils import serializer_helpers
 # third party imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+
 from .serializers import PostSerializer
 from .models import Post
 
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
         serializer = PostSerializer(qs, many=True)
--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
+    "rest_auth",
     "core",
 ]
 
@@ -128,3 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication"
+    ]
+}
